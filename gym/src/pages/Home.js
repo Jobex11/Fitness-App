7408bc853f1fccd157ce5d3a6286d0f04344eca0
jobex11 <joblawal33@gmail.com>
2024-01-05 01:45:21 +0100

    added ExcerciseCard

--- gym/src/pages/Home.js
+++ gym/src/pages/Home.js
@@ -5,11 +5,21 @@ import Exercises from "../components/Exercises";
 import SearchExercises from "../components/SearchExercises";
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        exercises={exercises}
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+      />
     </Box>
   );
 };
